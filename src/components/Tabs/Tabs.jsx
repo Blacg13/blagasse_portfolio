b55921313aa80d19c6c8a408a3cb2d338603237d
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-02-25 18:34:03 +0100

    Added content

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -8,10 +8,10 @@ const Tabs = () => {
       id: 1,
       tabTopic: 'about',
       tabTitle: 'about me',
-      img: undefined,
+      tabImg: undefined,
       tabContent: [
-        "I'm passionate about programming and designing intuitive website interfaces",
-        "Currently training to become a frontenddeveloper, I'm diving deep into JavaScript and React with a relentless curiosity to master and optimize every line of code.",
+        "I'm passionate about programming and designing intuitive website interfaces. ",
+        "Currently training to become a frontenddeveloper, I'm diving deep into JavaScript and React with a relentless curiosity to master and optimize every line of code. ",
       ],
     },
     {
@@ -20,21 +20,10 @@ const Tabs = () => {
       tabTitle: 'rock paper scissors',
       projectDate: new Date(2024, 2),
       projectLanguages: ['HTML', 'CSS', 'JavaScript'],
-      img: undefined,
-      tabContent: ['bloub', 'blabb'],
+      tabImg: 'rock-paper-scissors.png',
+      tabContent: ['\"Rock, paper, scissors\” game made during my training as a frontend developer at Interface3 (Brussels, Belgium). ', 'The user clicks on one of the images on the left, and the computer randomly selects one of the images with a small animation. '],
       githubProjectLink: 'https://github.com/Blacg13/IF3_PierrePapierCiseaux',
       liveProjectLink: 'https://www.whatbeatsrock.com/',
-    },
-    {
-      id: 3,
-      tabTopic: 'project',
-      tabTitle: 'Integrity Project',
-      projectDate: new Date(2025, 1),
-      projectLanguages: ['WordPress', 'React', 'Scss'],
-      img: undefined,
-      tabContent: ['bloub', 'blabb'],
-      githubProjectLink: null,
-      liveProjectLink: 'https://www.integrityproject.be/',
     },
     {
       id: 4,
@@ -42,18 +31,9 @@ const Tabs = () => {
       tabTitle: 'FestivalFlow',
       projectDate: new Date(2024, 10),
       projectLanguages: ['nodeJS', 'React', 'MongoDB', 'CSS'],
-      img: undefined,
-      tabContent: ['bloub', 'blabb'],
+      tabImg: "festival-flow.png",
+      tabContent: ['The ambition of this website is to streamline the management of exhibitors for a small festival in Wallonia, replacing spreadsheets, emails, and physical files. ', 'It centralizes contact management, contract tracking, and payment follow-ups while aiming to automate tasks like reminder emails. ', 'The key priorities are enabling contact, organizing data, and ensuring data communication. '],
       githubProjectLink: 'https://github.com/Blacg13/FestivalFlow',
-      liveProjectLink: null,
-    },
-    {
-      id: 5,
-      tabTopic: 'about',
-      tabTitle: 'gitignore',
-      img: undefined,
-      tabContent: ['bloub', 'blabb'],
-      githubProjectLink: null,
       liveProjectLink: null,
     },
   ];
@@ -69,6 +49,7 @@ const Tabs = () => {
             title={tab.tabTitle}
             date={tab.projectDate}
             language={tab.projectLanguages}
+            img={tab.tabImg}
             content={tab.tabContent}
             githubLink={tab.githubProjectLink}
             liveLink={tab.liveProjectLink}
@@ -103,6 +84,7 @@ const Tab = ({
     config: { mass: 1, tension: 200, friction: 20 },
     delay: isActive ? 100 : 0,
   });
+  
   return (
     <section id={`${tabId}_${topic}`} className={style[`${topic}`]}>
       <animated.div
@@ -121,7 +103,7 @@ const Tab = ({
         </div>
       </animated.div>
       <animated.div className={style['tab-content']} style={fade}>
-        {img ? <img src={img} alt={`view of ` + title} /> : null}
+        {img ? <img src={`/src/assets/img/${img}`} alt={`view of ` + title} /> : null}
         <p>{content}</p>
         <div className={style['tab-links']}>
           {githubLink ? <a href={githubLink} target='_blank' rel='noopener noreferrer'>
