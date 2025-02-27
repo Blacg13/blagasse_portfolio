57fad8f8947d88f7c305e6f2b7d3bd46dde9f0d4
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-02-27 17:47:45 +0100

    Filled Secret Tab component + style

--- src/components/Tabs/SecretTab.jsx
+++ src/components/Tabs/SecretTab.jsx
@@ -1,11 +1,44 @@
 import style from './Tabs.module.css';
+import { useState } from 'react';
+import { animated, useSpring } from '@react-spring/web';
 
 const SecretTab = () => {
-
+    const [isActive, setActive] = useState(false);
+      // animations ----------------------------------
+      const spin = useSpring({
+        transform: `rotateY(${isActive ? 180 : 0}deg)`,
+        config: { mass: 3, tension: 130, friction: 30 },
+      });
+      const fade = useSpring({
+        opacity: isActive ? 1 : 0,
+        display: isActive ? 'block' : 'none',
+        transform: `translateY(${isActive ? 0 : 20}px)`,
+        config: { mass: 5, tension: 150, friction: 50 },
+        delay: isActive ? 200 : 0,
+      });
+    //------------------------------------------------
 
     return (
-        <section id={"secretTab"} className={style.about}>
-            <h3>Hello world</h3>
+        <section id={"secretTab"} className={style["secret-tab"]}>
+            <animated.div
+          className={style['tab-spin-title']}
+          style={spin}
+          onClick ={() => setActive(!isActive)}
+            >
+          <div className={style['flip-card-container']}>
+            <div
+              className={
+                isActive ? style['flip-card-recto'] : style['flip-card-verso']
+              }
+            >
+              <h3>Gitignore</h3>
+            </div>
+          </div>
+        </animated.div>
+        <animated.div className={style['tab-content']} style={fade}>
+          
+          <p>hello world</p>
+        </animated.div>
         </section>
     )
 }
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Tab from "./Tab.jsx"
 import SecretTab from "./SecretTab.jsx"
+import { useSpring } from '@react-spring/web';
 
 const Tabs = () => {
   const tabs = [
@@ -50,6 +51,25 @@ const Tabs = () => {
 
     }
   };
+      // animations ----------------------------------
+      const [isActive, setActive] = useState(false);
+      const handleActiv = () => {
+        setActive(!isActive)
+        return isActive
+      }
+      const spin = useSpring({
+        transform: `rotateY(${isActive ? 180 : 0}deg)`,
+        config: { mass: 3, tension: 130, friction: 30 },
+      });
+      const fade = useSpring({
+        opacity: isActive ? 1 : 0,
+        display: isActive ? 'block' : 'none',
+        transform: `translateY(${isActive ? 0 : 20}px)`,
+        config: { mass: 5, tension: 150, friction: 50 },
+        delay: isActive ? 200 : 0,
+      });
+    //------------------------------------------------
+
   return (
     <>
       {tabs.map((tab) => {
@@ -71,7 +91,11 @@ const Tabs = () => {
           />
         );
       })}
-      <SecretTab/>
+      <SecretTab
+      animationTitle={spin}
+      animationContent= {fade}
+      handleClick={handleActiv}
+      isActive={isActive} />
     </>
   );
 };
