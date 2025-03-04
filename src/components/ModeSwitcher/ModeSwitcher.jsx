2d5ef8c06e0e91200e025bfff2c85e79c56877b8
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-03-04 21:16:34 +0100

    Added state to theme toggle

--- src/components/ModeSwitcher/ModeSwitcher.jsx
+++ src/components/ModeSwitcher/ModeSwitcher.jsx
@@ -1,18 +1,22 @@
 import style from './ModeSwitcher.module.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 const ModeSwitcher = () => {
   const HTML = useRef(document.documentElement);
+  localStorage.setItem("theme", HTML.current.getAttribute("data-theme"));
   const switchTheme = () => {
     const currentTheme = HTML.current.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     HTML.current.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
+  const handleClick = () => {
+    switchTheme();
+    setTheme(theme === "dark" ? "light" : "dark");
   }
   return (
-    <button className={style['mode-switcher']} onClick={switchTheme}>
-      <label>
-      <input type="checkbox"/>
-      <span className={style.slider}></span>
-      </label>
+    <button className={style['mode-switcher']} onClick={handleClick}>
+      <div className={HTML.current.getAttribute("data-theme") === "dark" ? style['moon'] : style['sun']}></div>
     </button>
   );
 };
