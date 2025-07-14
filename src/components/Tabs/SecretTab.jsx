8063a040bba753f9446d5d52cd02a5579847446e
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-07-14 16:49:56 +0200

    refactor: secret tab

--- src/components/Tabs/SecretTab.jsx
+++ src/components/Tabs/SecretTab.jsx
@@ -1,7 +1,6 @@
 import style from './Tabs.module.css';
 import { useState } from 'react';
 import { animated, useSpring } from '@react-spring/web';
-import ContactForm from './ContactForm';
 
 const SecretTab = ({disclosure}) => {
     const [isActive, setActive] = useState(false);
@@ -23,21 +22,6 @@ const SecretTab = ({disclosure}) => {
         config: { mass: 5, tension: 150, friction: 50 },
         delay: disclosure ? 200 : 0,
       });
-      //------------------------------------------------
-    // // form --------------------------------------------
-    // const handleSubmit = (e) => {
-    //   e.preventDefault();
-    //   const form = e.target;
-    //   const formData = new FormData(form);
-    //   const firstname = formData.get('firstname');
-    //   const lastname = formData.get('lastname');
-    //   const email = formData.get('email');
-    //   // const phone = formData.get('phone');
-    //   // const object = formData.get('object');
-    //   const message = formData.get('message');
-    //   console.log(firstname, lastname, email, message);
-    // }
-    //--------------------------------------------------
     return (
       <animated.section id={"secretTab"} 
         className={style["secret-tab"]} 
@@ -59,23 +43,7 @@ const SecretTab = ({disclosure}) => {
           </div>
         </animated.div>
         <animated.div className={style['tab-content']} style={fade}>
-          <p>If you are interested in my work, don't hesitate to contact me : </p>
-          {/* <form onSubmit={handleSubmit} className={style['contact-form']}>
-            <div>
-              <div>
-                <input name='firstname' placeholder='First name' type="text" required/>
-                <input name='lastname' placeholder='Last name' type="text" required/>
-              </div>
-              <div>
-                <input name='email' placeholder='Email' type="text" required/>
-                <input name='phone' placeholder='Phone number' type="text" />
-              </div>
-            </div>
-              <input name='object' placeholder='Object' type="text" required/>
-              <textarea name='message' placeholder='Message' rows='6' className={style['message']} required/>
-              <button type='submit' className={style['send-button']}>Send</button>
-          </form> */}
-          <ContactForm />
+              <p>jfjkd</p>
         </animated.div>
         </animated.section>
     )
